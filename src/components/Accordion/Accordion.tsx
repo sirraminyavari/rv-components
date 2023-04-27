1210714657047240d1d997c6f79127219b82c6ec
Soren Abedi <soren.abedi@gmail.com>
2023-04-27 13:51:21 +0330

    feat: :sparkles: improve `SidebarSubMenu` actions to comply with supplied ***activeLink***  prop
    Signed-off-by: Soren Abedi <soren.abedi@gmail.com>

--- src/components/Accordion/Accordion.tsx
+++ src/components/Accordion/Accordion.tsx
@@ -8,6 +8,8 @@ import {
   PropsWithoutRef,
   ReactNode,
   useCallback,
+  useEffect,
+  useRef,
   useState,
 } from 'react';
 import AnimateHeight from 'react-animate-height';
@@ -22,10 +24,13 @@ export interface RVAccordion
     >,
     'color'
   > {
+  id?: string;
   color?: RVColorProp;
   label?: ReactNode;
   open?: boolean;
   defaultOpen?: boolean;
+  activeLabel?: boolean;
+  onTriggerButtonLoad?: (element: HTMLButtonElement) => void;
   onLabelClick?: MouseEventHandler<HTMLButtonElement>;
   onAccordionStatusChange?: (
     event: MouseEvent<HTMLButtonElement, globalThis.MouseEvent>,
@@ -37,6 +42,7 @@ export interface RVAccordion
 const Accordion = forwardRef<HTMLDivElement, RVAccordion>(
   (
     {
+      id,
       children,
       className,
       color = RVColorProp.gray,
@@ -45,13 +51,16 @@ const Accordion = forwardRef<HTMLDivElement, RVAccordion>(
       dir,
       defaultOpen = false,
       onLabelClick,
+      onTriggerButtonLoad,
       onAccordionStatusChange,
       labelClassName,
+      activeLabel,
       ...props
     },
     ref
   ) => {
     const [isOpen, setIsOpen] = useState(open || defaultOpen);
+    const accordionTriggerButtonRef = useRef<HTMLButtonElement>(null);
     const triggerAccordion: MouseEventHandler<HTMLButtonElement> = useCallback(
       (event) => {
         setIsOpen((prevState) => {
@@ -62,6 +71,10 @@ const Accordion = forwardRef<HTMLDivElement, RVAccordion>(
       },
       [open, onLabelClick]
     );
+    useEffect(() => {
+      if (!onTriggerButtonLoad || !accordionTriggerButtonRef.current) return;
+      onTriggerButtonLoad(accordionTriggerButtonRef.current);
+    }, [onTriggerButtonLoad, accordionTriggerButtonRef]);
 
     return (
       <div
@@ -71,12 +84,20 @@ const Accordion = forwardRef<HTMLDivElement, RVAccordion>(
         {...props}
       >
         <button
+          ref={accordionTriggerButtonRef}
           className={clsx(styles.triggerButton, labelClassName)}
           onClick={triggerAccordion}
           aria-label="accordion-trigger"
+          data-id={id}
         >
           <CaretSvg direction={'right'} />
-          <span className={styles.triggerButtonLabel} onClick={onLabelClick}>
+          <span
+            className={clsx(
+              styles.triggerButtonLabel,
+              activeLabel && styles.triggerButtonLabelActive
+            )}
+            onClick={onLabelClick}
+          >
             {' '}
             {label}
           </span>
