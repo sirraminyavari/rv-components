2da5bb37876317b02f88ec8233ccd91959194628
Soren Abedi <soren.abedi@gmail.com>
2023-09-08 13:12:30 +0330

    chore: :memo: add data typings for `Checkbox` and `Switch`
    Signed-off-by: Soren Abedi <soren.abedi@gmail.com>

--- src/components/Switch/Switch.stories.tsx
+++ src/components/Switch/Switch.stories.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 
 import { Switch as SwitchComponent, RVSwitch } from '.';
-import { ShapesSvg } from '../../icons';
+import { ThemeBlock } from '../../storybookComponents';
+import { RVSizeProp, RVVariantProp } from '../../types';
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
@@ -13,21 +14,72 @@ export default {
 } as ComponentMeta<typeof SwitchComponent>;
 
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
-const Template: ComponentStory<typeof SwitchComponent> = ({ label = 'label', ...args }) => (
-  <SwitchComponent label={label} {...args} />
-);
+const Template: ComponentStory<typeof SwitchComponent> = ({
+  label = 'label',
+  color,
+  variant,
+  ...args
+}) => {
+  const [colorClass, setColorClass] = useState(color);
+  useEffect(() => {
+    setColorClass(color);
+  }, [color]);
+  return (
+    <ThemeBlock onColorChange={setColorClass}>
+      <div>
+        <SwitchComponent
+          label={label}
+          variant={variant || RVVariantProp.primary}
+          color={colorClass}
+          {...args}
+        />
+      </div>
+      <div>
+        <SwitchComponent
+          label={label}
+          variant={variant || RVVariantProp.outline}
+          color={colorClass}
+          {...args}
+        />
+      </div>
+      <div>
+        <SwitchComponent
+          label={label}
+          variant={variant || RVVariantProp.white}
+          color={colorClass}
+          {...args}
+        />
+      </div>
+    </ThemeBlock>
+  );
+};
+
+export const ShowCase = ({ label = 'label', color, variant, ...args }) => {
+  return (
+    <>
+      <SwitchComponent label={label} variant={variant} color={color} {...args} />
+    </>
+  );
+};
 
 // More on args: https://storybook.js.org/docs/react/writing-stories/args
-export const Switch = Template.bind({});
+export const checked = Template.bind({});
+checked.args = { checked: true };
 
-export const primary = Template.bind({});
-primary.args = { variant: 'primary' };
+export const SudoHover = Template.bind({});
+SudoHover.args = { sudoAction: 'hover' };
 
-export const outline = Template.bind({});
-outline.args = { variant: 'outline' };
+export const SudoFocus = Template.bind({});
+SudoFocus.args = { sudoAction: 'focus' };
 
-export const white = Template.bind({});
-white.args = { variant: 'white' };
+export const activeState = Template.bind({});
+activeState.args = { checked: true };
 
-export const disabled = Template.bind({});
-disabled.args = { disabled: true };
+export const SmallSized = Template.bind({});
+SmallSized.args = { size: RVSizeProp.small };
+
+export const MediumSized = Template.bind({});
+MediumSized.args = { size: RVSizeProp.medium };
+
+export const LargeSized = Template.bind({});
+LargeSized.args = { size: RVSizeProp.large };
