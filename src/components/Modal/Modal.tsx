f7d5a38fb598db775dfdc045e00bf1de65b14bbe
Soren Abedi <soren.abedi@gmail.com>
2023-09-15 08:41:32 +0330

    chore: :memo: improve `Modal` storybook stories
    Signed-off-by: Soren Abedi <soren.abedi@gmail.com>

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,12 +1,14 @@
 import clsx from 'clsx';
-import { CSSProperties, forwardRef } from 'react';
+import { CSSProperties, forwardRef, useCallback, useRef } from 'react';
 import ReactModal, { Props as ModalProps } from 'react-modal';
 import mergeWith from 'lodash/mergeWith';
 import { RVColorProp, RVSizeProp } from '../../types';
 import styles from './Modal.module.scss';
 
 export interface RVModal extends ModalProps {
+  /** set the component color palette (default:RVColorProp.cgBlue) */
   color?: RVColorProp;
+  /** set the size of the component (default:RVSizeProp.small) */
   size?: RVSizeProp;
 }
 
@@ -20,19 +22,40 @@ const Modal = forwardRef<ReactModal, RVModal>(
       closeTimeoutMS = 200,
       style,
       children,
+      isOpen,
+      parentSelector,
       ...props
     },
     ref
   ) => {
+    const elementID = `modal-${Date.now()}`;
+    const appElement = useRef<HTMLElement>();
+    const parentSelectorCallback = useCallback(() => {
+      if (parentSelector) return parentSelector();
+      if (appElement.current) return appElement.current!;
+      console.log('no !!!');
+      if (document.getElementById(elementID)) return document.getElementById(elementID)!;
+      return document.body;
+    }, [appElement.current, isOpen]);
+
     return (
-      <>
+      <div
+        id={elementID}
+        ref={(elRef) => {
+          if (elRef) {
+            appElement.current = elRef;
+          }
+        }}
+      >
         <ReactModal
           ref={ref}
+          appElement={appElement.current}
           portalClassName={'RVPortal'}
           overlayClassName={clsx(styles.modalOverlay, color, overlayClassName)}
           className={clsx(styles.modalContent, styles[size], className)}
           closeTimeoutMS={closeTimeoutMS}
-          parentSelector={() => document.body}
+          parentSelector={parentSelectorCallback}
+          isOpen={isOpen}
           style={mergeWith(
             {
               overlay: {
@@ -48,7 +71,7 @@ const Modal = forwardRef<ReactModal, RVModal>(
         >
           {children}
         </ReactModal>
-      </>
+      </div>
     );
   }
 );
