fdeff5ba7d60df548a71ccb8690cb16cf70b9081
Soren Abedi <soren.abedi@gmail.com>
2023-10-22 13:13:14 +0330

    feat: :sparkles: bootstrap `ServerManagementEmptyState` view
    Signed-off-by: Soren Abedi <soren.abedi@gmail.com>

--- src/views/server-management/index.ts
+++ src/views/server-management/index.ts
@@ -1,3 +1,5 @@
 export * from './server-management-admin-empty-state';
 export * from './server-management-admin-list';
 export * from './server-management-admin-new-server';
+
+export * from './server-management-empty-state';
