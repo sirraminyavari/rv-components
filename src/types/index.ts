2b3d03575e17c059504269cb4f9c3a7010804c80
ramin <sir.raminyavari@gmail.com>
2024-08-06 00:11:25 +1000

    fix: Export type ZServerResultEntity

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,2 +1,4 @@
 export * from './global';
 export * from './ts-extend';
+
+export type { ZServerResultEntity } from '../views/server-management/server-management-search-panel/ServerManagementSearchPanel';
